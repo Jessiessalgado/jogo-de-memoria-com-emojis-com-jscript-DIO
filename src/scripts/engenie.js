6dff50781d5391fd44b1391088477dfdc587188b
Jessiessalgado <jessica.silvasalgado@icloud.com>
2024-11-14 17:15:06 -0300

    dando efeito de rotate nos cards

--- src/scripts/engenie.js
+++ src/scripts/engenie.js
@@ -1,3 +1,4 @@
+// emojis
 const emojis = [
     "😍",
     "😍",
@@ -18,10 +19,25 @@ const emojis = [
 ];
 let openCards = [];
 
-
+// organizando os emojis em caixas.
 let shuffleEmojis = emojis.sort(() => Math.random() - 0.5 ? 2 : -1);
-
 for(let i=0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
 }
+
+// comportamento dos cards
+function handleClick(){
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500)
+    }
+}
+function checkMatch(){}
+
