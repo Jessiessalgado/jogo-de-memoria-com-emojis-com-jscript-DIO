6cee0915b4a14d2223301b15959cc33561dd3084
Jessiessalgado <jessica.silvasalgado@icloud.com>
2024-11-14 16:37:30 -0300

    corrigindo o erro de sintaxe no background

---
+++ src/scripts/engenie.js
@@ -0,0 +1,27 @@
+const emojis = [
+    "😍",
+    "😍",
+    "😂",
+    "😂",
+    "😘",
+    "😘",
+    "😎",
+    "😎",
+    "😜",
+    "😜",
+    "😉",
+    "😉",
+    "😨",
+    "😨",
+    "🤪",
+    "🤪"
+];
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() => Math.random() - 0.5 ? 2 : -1);
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+}
