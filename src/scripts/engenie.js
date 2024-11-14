cab0dc4675f044b37749dd4b60bfb4b9fcb40deb
Jessiessalgado <jessica.silvasalgado@icloud.com>
2024-11-14 17:42:25 -0300

    corrigindo o erro no controle das cartas abertas

--- src/scripts/engenie.js
+++ src/scripts/engenie.js
@@ -1,27 +1,13 @@
 // emojis
 const emojis = [
-    "😍",
-    "😍",
-    "😂",
-    "😂",
-    "😘",
-    "😘",
-    "😎",
-    "😎",
-    "😜",
-    "😜",
-    "😉",
-    "😉",
-    "😨",
-    "😨",
-    "🤪",
-    "🤪"
+    "😍", "😍", "😂", "😂", "😘", "😘", "😎", "😎",
+    "😜", "😜", "😉", "😉", "😨", "😨", "🤪", "🤪"
 ];
 let openCards = [];
 
 // organizando os emojis em caixas.
-let shuffleEmojis = emojis.sort(() => Math.random() - 0.5 ? 2 : -1);
-for(let i=0; i < emojis.length; i++) {
+let shuffleEmojis = emojis.sort(() => Math.random() - 0.5);
+for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
@@ -30,30 +16,30 @@ for(let i=0; i < emojis.length; i++) {
 }
 
 // comportamento dos cards
-function handleClick(){
-    if(openCards.length < 2) {
+function handleClick() {
+    if (openCards.length < 2) {
         this.classList.add("boxOpen");
+        openCards.push(this);  // Adiciona a carta aberta ao array
     }
 
-    if(openCards.length == 2){
-        setTimeout(checkMatch, 500)
+    if (openCards.length === 2) {
+        setTimeout(checkMatch, 500);
     }
 }
 
 // Criando a condição de vitória
-function checkMatch(){
-    if(openCards[0].innerHTML === openCards[1].innerHTML){
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
         openCards[0].classList.add("boxMatch");
         openCards[1].classList.add("boxMatch");
-    }else {
+    } else {
         openCards[0].classList.remove("boxOpen");
         openCards[1].classList.remove("boxOpen");
     }
 
-    openCards = [];
+    openCards = []; // Limpa o array após a verificação
 
-    if(document.querySelectorAll(".boxMatch").length === emojis.length) {
-        alert("Você venceu !");
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu!");
     }
 }
-
